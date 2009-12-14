d9bcdfa1f57f8984e725d5fdddce5f915ff2acf2
Red Forks <redforks@gmail.com>
2009-12-14 10:28:38 +0800

    git web, can handle some unicode char(sometime still cannot convert).

--- web/cgi-bin/convert.py
+++ web/cgi-bin/convert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import cgitb, cgi
+import cgitb, cgi, re
 import docutils, os
 import rstslide_template
 
@@ -9,14 +9,23 @@ import rstslide
 
 cgitb.enable()
 
+def get_input():
+    import struct
+    from codecs import decode
+    def from_raw(match):
+        s = match.group(1)
+        buf = struct.pack('i', int(s))
+        return decode(buf, 'utf-16')
+    form = cgi.FieldStorage()
+    result = unicode(form['rst'].value)
+    return re.sub('&#([0-9]+);', from_raw, result)
+
 logfile = open('/tmp/slide.log', 'at')
 
-form = cgi.FieldStorage()
 
 parser = docutils.parsers.rst.Parser()
 document = docutils.utils.new_document('rstslide', rstslide.Settings())
-input = form['rst'].value
-parser.parse(input, document)
+parser.parse(get_input(), document)
 
 pdffile = os.tmpnam()
 document.walkabout(rstslide.Visitor(rstslide_template, document, pdffile))
