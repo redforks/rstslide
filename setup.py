d1e8abb4b828d3d2ca91995da51e6f50d49dc8dd
Red Forks <redforks@gmail.com>
2009-12-14 09:34:01 +0800

    setup.py: install extra files to /usr/share/rstslide

--- setup.py
+++ setup.py
@@ -11,4 +11,7 @@ setup(
         author_email='redforks@gmail.com',
         py_modules=['rstslide', 'rstslide_template'],
         requires=['pycairo', 'docutils', 'pil'],
+        data_files = [('/usr/share/rstslide', ['slide-test.rst',
+            'slide-test-en.rst', 'bwp_template.py',
+            'angel-star.png', 'snow-angel.gif', 'sleep.jpg', 'title.png', 'README'])]
     )
