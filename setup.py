53f9205a59c967c35fecf9003228f32f6a58532f
Red Forks <redforks@gmail.com>
2009-12-14 09:42:17 +0800

    setup.py: add web support files

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
         author_email='redforks@gmail.com',
         py_modules=['rstslide', 'rstslide_template'],
         requires=['pycairo', 'docutils', 'pil'],
-        data_files = [('/usr/share/rstslide', ['slide-test.rst',
+        data_files = [('/usr/share/rstslide/', ['slide-test.rst',
             'slide-test-en.rst', 'bwp_template.py',
-            'angel-star.png', 'snow-angel.gif', 'sleep.jpg', 'title.png', 'README'])]
+            'angel-star.png', 'snow-angel.gif', 'sleep.jpg', 'title.png',
+            'README']),
+            ('/usr/share/rstslide/web/', ['web/index.html']),
+            ('/usr/share/rstslide/web/cgi-bin/', ['web/cgi-bin/convert.py'])]
     )
